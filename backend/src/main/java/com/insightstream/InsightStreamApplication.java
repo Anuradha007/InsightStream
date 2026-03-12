fdd58d50d7fc9ded9a6316e3e7d89bb68e89ac78
bhushangolani <brgolani21@gmail.com>
2026-03-12 10:41:21 -0400

    Add Spring Boot backend skeleton and main application

---
+++ backend/src/main/java/com/insightstream/InsightStreamApplication.java
@@ -0,0 +1,12 @@
+package com.insightstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsightStreamApplication.class, args);
+    }
+}
